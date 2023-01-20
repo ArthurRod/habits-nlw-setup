0f252e3005ed7f8aefb11c7bf5614de4239736db
Arthur Rodrigues <arthur9713@hotmail.com>
2023-01-20 20:35:50 -0300

    Função criar hábito finalizada

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -2,32 +2,49 @@ import * as Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
 import { FormEvent, useState } from "react";
 import { AVAILABLE_WEEK_DAYS } from "../constants/availableWeekDays";
+import { api } from "../lib/axios";
 
 export function NewHabitForm() {
-  const [title, setTitle] = useState("")
-  const [weekDays, setWeekDays] = useState<number[]>([])
+  const [title, setTitle] = useState("");
+  const [weekDays, setWeekDays] = useState<number[]>([]);
 
   function createNewHabit(event: FormEvent) {
-    event.preventDefault()
+    event.preventDefault();
 
-    console.log(title, weekDays)
-    setTitle("")
+    if (!title || weekDays.length === 0) {
+      return;
+    }
+
+    api
+      .post("habits", {
+        title,
+        weekDays,
+      })
+      .catch((error) => console.log(error));
+
+    alert("Hábito criado com sucesso!");
+
+    setTitle("");
+    setWeekDays([]);
   }
 
   function handleToggleWeekDay(weekDay: number) {
-    if(weekDays.includes(weekDay)) {
-      const weekDaysWithRemovedOne = weekDays.filter(day => day !== weekDay)
+    if (weekDays.includes(weekDay)) {
+      const weekDaysWithRemovedOne = weekDays.filter((day) => day !== weekDay);
 
-      setWeekDays(weekDaysWithRemovedOne)
+      setWeekDays(weekDaysWithRemovedOne);
     } else {
-      const weekDaysWithAddedOne = [...weekDays, weekDay]
+      const weekDaysWithAddedOne = [...weekDays, weekDay];
 
-      setWeekDays(weekDaysWithAddedOne)
+      setWeekDays(weekDaysWithAddedOne);
     }
   }
 
   return (
-    <form onSubmit={(e) => createNewHabit(e)} className="w-full flex flex-col mt-6">
+    <form
+      onSubmit={(e) => createNewHabit(e)}
+      className="w-full flex flex-col mt-6"
+    >
       <label htmlFor="title" className="font-semibold leading-tight">
         Qual seu comprometimento?
       </label>
@@ -35,11 +52,11 @@ export function NewHabitForm() {
       <input
         type="text"
         id="title"
-        value={title.length > 0 ? title : ""}
+        value={title}
         placeholder="ex.: Exercícios, dormir bem, etc..."
         className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
         autoFocus
-        onChange={event => setTitle(event.target.value)}
+        onChange={(event) => setTitle(event.target.value)}
       />
 
       <label htmlFor="" className="font-semibold leading-tight mt-4">
@@ -52,6 +69,7 @@ export function NewHabitForm() {
             <Checkbox.Root
               key={weekDay}
               className="flex items-center gap-3 group"
+              checked={weekDays.includes(i)}
               onCheckedChange={() => handleToggleWeekDay(i)}
             >
               <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
