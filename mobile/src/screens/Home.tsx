4dc10c1024a90e87e2bcc9aebd34b7aae587a3cf
Arthur Rodrigues <arthur9713@hotmail.com>
2023-01-21 14:22:35 -0300

    App mobile finalizado

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import { Text, View, ScrollView, Alert } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+
 import { api } from "../lib/axios";
 import dayjs from "dayjs";
 
@@ -42,9 +43,11 @@ export function Home() {
     }
   }
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  useFocusEffect(
+    useCallback(() => {
+      fetchData();
+    }, [])
+  );
 
   if (loading) {
     return <Loading />;
